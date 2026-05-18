261db59fe4ea5c98cdd0b731c3d51e3a9d9d6387
TheRealToxicDev <toxic.dev09@gmail.com>
2026-05-18 10:09:17 -0600

    feat(add): more workflow fixes!

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            [
-                'feat',
-                'fix',
-                'docs',
-                'chore',
-                'style',
-                'refactor',
-                'ci',
-                'test',
-                'perf',
-                'patch',
-                'revert',
-                'vercel',
-                'update',
-                'add'
-            ]
-        ]
-    }
-};
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'perf',
+        'patch',
+        'revert',
+        'vercel',
+        'update',
+        'add',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'never'],
+  },
+}
